c4331fa9ccdb173cdc0b16250e481df8d35272e0
Houssem Nasri <housi.housi2015@gmail.com>
2022-12-01 01:26:45 +0100

    Initial commit

---
+++ src/main/scala/Utils.scala
@@ -0,0 +1,26 @@
+package tech.houssemnasri
+
+import java.io.File
+import java.nio.file.Files
+import scala.io.Source
+
+/**
+ * Reads and Returns the lines of advent of code input for the given day
+ * */
+def readInput(dayNumber: Int): List[String] = {
+  val inputFile = new File(s"./src/main/scala/day0$dayNumber/Day0$dayNumber.txt")
+  if (!inputFile.exists()) {
+    throw IllegalStateException(s"Couldn't find the input file at ${inputFile.getAbsoluteFile}")
+  }
+
+  val inputSource = Source.fromFile(inputFile)
+  if (inputSource.isEmpty) {
+    inputSource.close()
+    throw new IllegalStateException(s"Input file of Day0$dayNumber is empty")
+  }
+
+  val inputLines = inputSource.getLines().toList
+  inputSource.close()
+
+  return inputLines
+}
---
+++ src/main/scala/day01/Day01.scala
@@ -0,0 +1,17 @@
+package tech.houssemnasri
+package day01
+
+@main
+def main(): Unit = {
+  def partOne(input: List[String]): Int = {
+    -1;
+  }
+
+  def partTwo(input: List[String]): Int = {
+    -1;
+  }
+
+  val input = readInput(1)
+  println(partOne(input))
+  println(partTwo(input))
+}
