c8a2be506abbe9e8aafa92d0d509bef2f199b902
Houssem Nasri <housi.housi2015@gmail.com>
2022-12-03 19:44:41 +0100

    Submit Day02 solution!

---
+++ src/main/scala/day02/Day02.scala
@@ -0,0 +1,48 @@
+package tech.houssemnasri
+package day02
+
+@main
+def main(): Unit = {
+
+  val rockPaperScissorsScoreLookup = Map(
+    ('A', 'X') -> 3,
+    ('A', 'Y') -> 6,
+    ('A', 'Z') -> 0,
+    ('B', 'X') -> 0,
+    ('B', 'Y') -> 3,
+    ('B', 'Z') -> 6,
+    ('C', 'X') -> 6,
+    ('C', 'Y') -> 0,
+    ('C', 'Z') -> 3)
+
+  def extractRoundInput(roundLine: String): (Char, Char) = {
+    val roundElements = roundLine.split(' ').map(_.charAt(0))
+    (roundElements(0), roundElements(1))
+  }
+
+  def partOne(input: List[String]): Int = {
+    val roundsInput = input.map(extractRoundInput)
+    var totalScore = 0
+    for (opponent: Char, player: Char) <- roundsInput do {
+      totalScore += player - 'X' + 1
+      totalScore += rockPaperScissorsScoreLookup((opponent, player))
+    }
+    totalScore
+  }
+
+  def partTwo(input: List[String]): Int = {
+    val strategyGuideRounds = input.map(extractRoundInput)
+    var totalScore = 0
+    for (opponent: Char, expectedResult: Char) <- strategyGuideRounds do {
+      val expectedPlayerScore = if (expectedResult == 'X') 0 else if (expectedResult == 'Y') 3 else 6
+      val player = rockPaperScissorsScoreLookup.find(p => p._1._1 == opponent && p._2 == expectedPlayerScore).get._1._2
+      totalScore += player - 'X' + 1
+      totalScore += rockPaperScissorsScoreLookup((opponent, player))
+    }
+    totalScore
+  }
+
+  val input = readInput(2)
+  println(partOne(input))
+  println(partTwo(input))
+}
